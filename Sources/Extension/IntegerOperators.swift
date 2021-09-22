426f07ebd900305e5c9a9e111f1deab1f4f88243
darvintang <input@tcoding.cn>
2021-09-22 14:49:16 +0800

    no message

--- Sources/Extension/IntegerOperators.swift
+++ Sources/Extension/IntegerOperators.swift
@@ -1,30 +1,36 @@
 //
 //  IntegerOperators.swift
-//  ObjectMapper
 //
-//  Created by Suyeol Jeon on 17/02/2017.
 //
-//  The MIT License (MIT)
+//  Created by darvintang on 2021/9/19.
 //
-//  Copyright (c) 2014-2018 Tristan Himmelman
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+
+/*
+
+ MIT License
+
+ Copyright (c) 2021 darvintang http://blog.tcoding.cn
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+
+ */
+
 
 import Foundation
 
@@ -79,21 +85,6 @@ public func <- <T: UnsignedInteger>(left: inout T?, right: Map) {
     default: ()
     }
 }
-
-// Code targeting the Swift 4.1 compiler and below.
-#if !(swift(>=4.1.50) || (swift(>=3.4) && !swift(>=4.0)))
-    /// ImplicitlyUnwrappedOptional UnsignedInteger mapping
-    public func <- <T: UnsignedInteger>(left: inout T!, right: Map) {
-        switch right.mappingType {
-        case .fromJSON where right.isKeyPresent:
-            let value: T! = toUnsignedInteger(right.currentValue)
-            FromJSON.basicType(&left, object: value)
-        case .toJSON:
-            left >>> right
-        default: ()
-        }
-    }
-#endif
 
 // MARK: - Casting Utils
 

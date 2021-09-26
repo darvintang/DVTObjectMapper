ddc19e7f2b7cf75bc1b904f5bbda48e47db29170
darvintang <input@tcoding.cn>
2021-09-26 16:57:49 +0800

    添加Decimal支持

---
+++ Sources/Extension/DecimalOperators.swift
@@ -0,0 +1,49 @@
+//
+//  DecimalOperators.swift
+//
+//
+//  Created by darvintang on 2021/9/26.
+//
+
+import Foundation
+
+// MARK: - Decimal
+
+/// Decimal mapping
+public func <- (left: inout Decimal, right: Map) {
+    switch right.mappingType {
+    case .fromJSON where right.isKeyPresent:
+        let value = toDecimal(right.currentValue) ?? 0
+        FromJSON.basicType(&left, object: value)
+    case .toJSON:
+        left >>> right
+    default: ()
+    }
+}
+
+/// Optional Decimal mapping
+public func <- (left: inout Decimal?, right: Map) {
+    switch right.mappingType {
+    case .fromJSON where right.isKeyPresent:
+        let value = toDecimal(right.currentValue)
+        FromJSON.basicType(&left, object: value)
+    case .toJSON:
+        left >>> right
+    default: ()
+    }
+}
+
+// MARK: - Casting Utils
+
+private func toDecimal(_ value: Any?) -> Decimal? {
+    guard
+        case let decimalValue as Decimal = value
+    else {
+        if let tempValue = value {
+            return Decimal(string: "\(tempValue)")
+        }
+        return nil
+    }
+
+    return decimalValue
+}
